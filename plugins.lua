3733b0051e26ad698ea0610c76c9e2936cab339e
Arin Faraj <arin.abdul99@gmail.com>
2023-12-25 12:45:50 +0300

    add copilot,dap,flutter

--- plugins.lua
+++ plugins.lua
@@ -1,10 +1,50 @@
-local overrides = require("custom.configs.overrides")
+local overrides = require "custom.configs.overrides"
 
 ---@type NvPluginSpec[]
 local plugins = {
-
   -- Override plugin definition options
+  {
+    "zbirenbaum/copilot.lua",
+    -- Lazy load when event occurs. Events are triggered
+    -- as mentioned in:
+    -- https://vi.stackexchange.com/a/4495/20389
+    event = "InsertEnter",
+    -- You can also have it load at immediately at
+    -- startup by commenting above and uncommenting below:
+    -- lazy = false
+    opts = overrides.copilot,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
+    },
+    opts = function(_, opts)
+      local cmp = require "cmp"
+      opts.completion = {
+        completeopt = "menu,menuone,noinsert,noselect",
+      }
+      opts.sources = {
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "copilot", group_index = 2 },
+        { name = "luasnip", group_index = 2 },
+        { name = "buffer", group_index = 2 },
+        { name = "nvim_lua", group_index = 2 },
+        { name = "path", group_index = 2 },
+      }
 
+      opts.mapping = cmp.mapping.preset.insert {
+        ["<CR>"] = cmp.mapping.confirm {
+          select = false,
+        },
+      }
+    end,
+  },
   {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -25,7 +65,7 @@ local plugins = {
   -- override plugin configs
   {
     "williamboman/mason.nvim",
-    opts = overrides.mason
+    opts = overrides.mason,
   },
 
   {
@@ -60,6 +100,280 @@ local plugins = {
   --   "mg979/vim-visual-multi",
   --   lazy = false,
   -- }
+  --
+  {
+    "mfussenegger/nvim-dap",
+
+    dependencies = {
+
+      -- fancy UI for the debugger
+      {
+        "rcarriga/nvim-dap-ui",
+      -- stylua: ignore
+      keys = {
+        { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+        { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+      },
+        opts = {},
+        config = function(_, opts)
+          -- setup dap config by VsCode launch.json file
+          -- require("dap.ext.vscode").load_launchjs()
+          local dap = require "dap"
+          local dapui = require "dapui"
+          dapui.setup(opts)
+          dap.listeners.after.event_initialized["dapui_config"] = function()
+            dapui.open {}
+          end
+          dap.listeners.before.event_terminated["dapui_config"] = function()
+            dapui.close {}
+          end
+          dap.listeners.before.event_exited["dapui_config"] = function()
+            dapui.close {}
+          end
+        end,
+      },
+
+      -- virtual text for the debugger
+      {
+        "theHamsta/nvim-dap-virtual-text",
+        opts = {},
+      },
+
+      -- which key integration
+      {
+        "folke/which-key.nvim",
+        optional = true,
+        opts = {
+          defaults = {
+            ["<leader>d"] = { name = "+debug" },
+          },
+        },
+      },
+
+      -- mason.nvim integration
+      {
+        "jay-babu/mason-nvim-dap.nvim",
+        dependencies = "mason.nvim",
+        cmd = { "DapInstall", "DapUninstall" },
+        opts = {
+          -- Makes a best effort to setup the various debuggers with
+          -- reasonable debug configurations
+          automatic_installation = true,
+
+          -- You can provide additional configuration to the handlers,
+          -- see mason-nvim-dap README for more information
+          handlers = {},
+
+          -- You'll need to check that you have the required things installed
+          -- online, please don't ask me how to install them :)
+          ensure_installed = {
+            -- Update this to ensure that you have the debuggers for the langs you want
+          },
+        },
+      },
+    },
+
+  -- stylua: ignore
+  keys = {
+    { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+    { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+    { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+    { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+    { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+    { "<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
+    { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+    { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+    { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+    { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+    { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+    { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+    { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+    { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+    { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+    { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+    { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+  },
+
+    config = function()
+      vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+  -- stylua: ignore
+  keys = {
+    { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+    { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+  },
+    opts = {},
+    config = function(_, opts)
+      -- setup dap config by VsCode launch.json file
+      -- require("dap.ext.vscode").load_launchjs()
+      local dap = require "dap"
+      local dapui = require "dapui"
+      dapui.setup(opts)
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open {}
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close {}
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close {}
+      end
+    end,
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    opts = {},
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = "mason.nvim",
+    cmd = { "DapInstall", "DapUninstall" },
+    opts = {
+      -- Makes a best effort to setup the various debuggers with
+      -- reasonable debug configurations
+      automatic_installation = true,
+
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
+      handlers = {},
+
+      -- You'll need to check that you have the required things installed
+      -- online, please don't ask me how to install them :)
+      ensure_installed = {
+        -- Update this to ensure that you have the debuggers for the langs you want
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    ft = { "dart" },
+    opts = {
+      setup = {
+        -- stylua: ignore
+        dartls = function() return true end,
+      },
+    },
+  },
+  {
+    "akinsho/flutter-tools.nvim",
+    ft = "dart",
+    -- stylua: ignore
+    enabled = not vim.o.diff,
+    dependencies = {
+      {
+        "dart-lang/dart-vim-plugin",
+        init = function()
+          vim.g.dart_style_guide = 2
+          vim.g.dart_html_in_string = true
+          vim.g.dart_trailing_comma_indent = true
+          vim.g.dartfmt_options = { "--fix" }
+        end,
+      },
+      "Nash0x7E2/awesome-flutter-snippets",
+    },
+    opts = {
+      ui = {
+        -- border = "single",
+        notification_style = "native",
+      },
+      widget_guides = {
+        enabled = true,
+      },
+      closing_tags = {
+        enabled = true,
+        prefix = "  ",
+      },
+      outline = {
+        open_cmd = "botright 40vnew",
+        auto_open = false,
+      },
+      dev_log = {
+        enabled = true,
+        open_cmd = "botright 5sp",
+      },
+      lsp = {
+        on_attach = function()
+          local register_keys = function()
+            local wk = require "which-key"
+            local bufnr = vim.api.nvim_get_current_buf()
+
+            wk.register({
+              o = { "<cmd>FlutterOutlineToggle<cr>", "Flutter Outline" },
+            }, {
+              prefix = "<leader>c",
+              buffer = bufnr,
+            })
+
+            wk.register({
+              r = { "<cmd>FlutterRun<cr>", "Flutter Run" },
+              R = { "<cmd>FlutterRestart<cr>", "Flutter Restart" },
+              p = { "<cmd>FlutterPubGet<cr>", "Flutter Pub Get" },
+              P = { "<cmd>FlutterPubUpgrade<cr>", "Flutter Pub Upgrade" },
+            }, {
+              prefix = "<leader>cD",
+              name = "+dart",
+              buffer = bufnr,
+            })
+          end
+
+          register_keys()
+          vim.api.nvim_create_autocmd("FileType", { pattern = "dart", callback = register_keys })
+
+          require("telescope").load_extension "flutter"
+        end,
+        color = {
+          enabled = true,
+          background = true,
+        },
+        settings = {
+          showTodos = false,
+          completeFunctionCalls = true,
+          updateImportsOnRename = true,
+          enableSnippets = true,
+          renameFilesWithClasses = true,
+        },
+      },
+      debugger = {
+        enabled = true,
+        run_via_dap = true,
+        exception_breakpoints = {},
+        register_configurations = function(_)
+          local dap = require "dap"
+          local flutterBin = vim.fn.resolve(vim.fn.exepath "flutter.bat")
+          local flutterSdk = vim.fn.fnamemodify(flutterBin, ":h:h")
+          local dartSdk = flutterSdk .. "\\bin\\cache\\dart-sdk"
+
+          dap.adapters.dart = {
+            type = "executable",
+            command = vim.fn.exepath "cmd.exe",
+            args = { "/c", flutterBin, "debug_adapter" },
+            options = {
+              detached = false,
+              initialize_timeout_sec = 10,
+            },
+          }
+
+          dap.configurations.dart = {
+            {
+              type = "dart",
+              request = "launch",
+              name = "Launch dart",
+              dartSdkPath = dartSdk,
+              flutterSdkPath = flutterSdk,
+              program = "${workspaceFolder}\\lib\\main.dart",
+              cwd = "${workspaceFolder}",
+            },
+          }
+
+          require("dap.ext.vscode").load_launchjs()
+        end,
+      },
+    },
+  },
 }
 
 return plugins
