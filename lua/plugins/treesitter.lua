e11afa44f834537d75f178bb29fde4e9a9d2a611
Arin Faraj <arin.abdul99@gmail.com>
2023-12-26 23:29:58 +0300

    remove dart indent in treesitter
    this fixes slow new line creation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
+    opts.indent = { enable = true, disable = { "dart" } }
     vim.list_extend(opts.ensure_installed, {
       "dart",
       "json",
