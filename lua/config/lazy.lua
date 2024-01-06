916d8aabc6b5842019f3a8151911eaaf6bdedb8c
Arin Faraj <arin.abdul99@gmail.com>
2024-01-06 09:10:25 +0300

    add rust and omnisharp

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.copilot" },
     -- { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.omnisharp" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
