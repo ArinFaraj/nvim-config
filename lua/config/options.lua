32a1ae757f04d60a8995f8433a6edfc155dbe89f
Arin Faraj <arin.abdul99@gmail.com>
2024-01-28 11:09:17 +0300

    disable shell override

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.shell = vim.fn.has("win32") > 0 and "nu"
-  or vim.fn.has("unix") > 0 and "bash"
-  or "bash"
+-- vim.opt.shell = vim.fn.has("win32") > 0 and "nu"
+--   or vim.fn.has("unix") > 0 and "bash"
+--   or "bash"
 vim.opt.scrolloff = 15 -- Lines of context
 -- vim.g.minipairs_disable = true
