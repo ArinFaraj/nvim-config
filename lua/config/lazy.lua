6a1cc912149a77980d9c45fd799e921df3f40845
Arin Faraj <arin.abdul99@gmail.com>
2024-02-14 11:51:14 +0300

    add more vscode settings

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -2,15 +2,19 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.vscode" },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+    },
+    -- { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     -- { import = "lazyvim.plugins.extras.lsp.none-ls" },
---
+++ lua/plugins/vscode.lua
@@ -0,0 +1,125 @@
+if not vim.g.vscode then
+  return {}
+end
+
+local enabled = {
+  "flit.nvim",
+  "lazy.nvim",
+  "leap.nvim",
+  "mini.ai",
+  -- "mini.comment",
+  "mini.pairs",
+  "mini.surround",
+  "nvim-treesitter",
+  "nvim-treesitter-textobjects",
+  "nvim-ts-context-commentstring",
+  "vim-repeat",
+  "LazyVim",
+}
+
+local Config = require("lazy.core.config")
+Config.options.checker.enabled = false
+Config.options.change_detection.enabled = false
+Config.options.defaults.cond = function(plugin)
+  ---@diagnostic disable-next-line: undefined-field
+  return vim.tbl_contains(enabled, plugin.name) or plugin.vscode
+end
+
+-- Add some vscode specific keymaps
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyVimKeymaps",
+  callback = function()
+    vim.keymap.set("n", "<leader><space>", "<cmd>Find<cr>")
+    vim.keymap.set(
+      "n",
+      "<leader>/",
+      [[<cmd>call VSCodeNotify('workbench.action.findInFiles')<cr>]]
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>ss",
+      [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]]
+    )
+    vim.keymap.set("x", "gc", "<Plug>VSCodeCommentary", {})
+    vim.keymap.set("n", "gc", "<Plug>VSCodeCommentary", {})
+    vim.keymap.set("o", "gc", "<Plug>VSCodeCommentary", {})
+    vim.keymap.set("n", "gcc", "<Plug>VSCodeCommentaryLine", {})
+    vim.keymap.set(
+      "n",
+      "<leader>ca",
+      [[<cmd>call VSCodeNotify('editor.action.quickFix')<cr>]]
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>bd",
+      [[<cmd>call VSCodeNotify('workbench.action.closeActiveEditor')<cr>]]
+    )
+    -- Got to next tab with shift+l and previous with shift+h
+    vim.keymap.set(
+      "n",
+      "<S-l>",
+      [[<cmd>call VSCodeNotify('workbench.action.nextEditor')<cr>]]
+    )
+    vim.keymap.set(
+      "n",
+      "<S-h>",
+      [[<cmd>call VSCodeNotify('workbench.action.previousEditor')<cr>]]
+    )
+    vim.keymap.set(
+      "n",
+      "C-j",
+      "<Cmd>call VSCodeNotify('workbench.action.navigateDown')<CR>"
+    )
+    vim.keymap.set(
+      "x",
+      "C-j",
+      "<Cmd>call VSCodeNotify('workbench.action.navigateDown')<CR>"
+    )
+    vim.keymap.set(
+      "n",
+      "C-k",
+      "<Cmd>call VSCodeNotify('workbench.action.navigateUp')<CR>"
+    )
+    vim.keymap.set(
+      "x",
+      "C-k",
+      "<Cmd>call VSCodeNotify('workbench.action.navigateUp')<CR>"
+    )
+    vim.keymap.set(
+      "n",
+      "C-h",
+      "<Cmd>call VSCodeNotify('workbench.action.navigateLeft')<CR>"
+    )
+    vim.keymap.set(
+      "x",
+      "C-h",
+      "<Cmd>call VSCodeNotify('workbench.action.navigateLeft')<CR>"
+    )
+    vim.keymap.set(
+      "n",
+      "C-l",
+      "<Cmd>call VSCodeNotify('workbench.action.navigateRight')<CR>"
+    )
+    vim.keymap.set(
+      "x",
+      "C-l",
+      "<Cmd>call VSCodeNotify('workbench.action.navigateRight')<CR>"
+    )
+  end,
+})
+
+return {
+  {
+    "LazyVim/LazyVim",
+    config = function(_, opts)
+      opts = opts or {}
+      -- disable the colorscheme
+      opts.colorscheme = function() end
+      require("lazyvim").setup(opts)
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { highlight = { enable = false } },
+  },
+}
