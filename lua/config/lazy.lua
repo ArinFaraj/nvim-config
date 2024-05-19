f7b258c1271c25b18ea1799a3d3a6984a856291f
Arin Faraj <arin.abdul99@gmail.com>
2024-05-19 09:57:54 +0300

    mini.ai is back to lazyvim core so removed from config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,7 +25,6 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.coding.mini-ai" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
