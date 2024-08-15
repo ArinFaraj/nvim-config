f9758f62b3382a3660e5ad285814f5e9330025a4
Arin Faraj <arin.abdul99@gmail.com>
2024-08-15 10:37:12 +0300

    improve notifications

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,6 +1,9 @@
 return {
   "rcarriga/nvim-notify",
   opts = {
-    background_colour = "#000000",
+    -- background_colour = "#000000",
+    top_down = false,
+    stages = "fade",
+    render = "compact",
   },
 }
