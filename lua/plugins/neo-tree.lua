75d5efad15bff1f684b0016e4d5568796b989113
Arin Faraj <arin.abdul99@gmail.com>
2024-09-16 17:33:53 +0300

    improve neotree open default folder

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,25 +5,26 @@ return {
       use_libuv_file_watcher = false,
     },
     window = {
-
       position = "right",
       mappings = {
         ["O"] = {
           command = function(state)
             local node = state.tree:get_node()
-            local filepath = node.path
-            local osType = os.getenv("OS")
+            local path = node:get_id()
+            -- macOs: open file in default application in the background.
+            vim.fn.jobstart({ "xdg-open", "-g", path }, { detach = true })
+            -- Linux: open file in default application
+            vim.fn.jobstart({ "xdg-open", path }, { detach = true })
 
-            local command
-
-            if osType == "Windows_NT" then
-              command = "start " .. filepath
-            elseif osType == "Darwin" then
-              command = "open " .. filepath
+            -- Windows: Without removing the file from the path, it opens in code.exe instead of explorer.exe
+            local p
+            local lastSlashIndex = path:match("^.+()\\[^\\]*$") -- Match the last slash and everything before it
+            if lastSlashIndex then
+              p = path:sub(1, lastSlashIndex - 1) -- Extract substring before the last slash
             else
-              command = "xdg-open " .. filepath
+              p = path -- If no slash found, return original path
             end
-            os.execute(command)
+            vim.cmd("silent !start explorer " .. p)
           end,
           desc = "open_with_system_defaults",
         },
