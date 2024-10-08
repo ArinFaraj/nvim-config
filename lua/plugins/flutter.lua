a07327a8861fe25cc2c1c07d3ffc91472da22965
Arin Faraj <arin.abdul99@gmail.com>
2024-10-08 10:49:49 +0300

    workaround for auto pausing of flutter debugging

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -225,10 +225,49 @@ return {
           run_via_dap = false,
           exception_breakpoints = {},
           register_configurations = function(_)
+            -- Lua script to automatically run DapContinue if debugger pauses within 10 seconds of start, with notifications
+            local dap = require("dap")
+
+            -- Variable to track when the debugging session starts
+            local debug_start_time = nil
+
+            -- Function to check and automatically resume debugger with notification
+            local function auto_resume_if_stopped()
+              -- Get the current time
+              local current_time = vim.loop.now()
+
+              -- Calculate the time difference between now and when the debugging session started
+              if
+                debug_start_time
+                and (current_time - debug_start_time) <= 600000
+              then
+                vim.defer_fn(function()
+                  dap.continue()
+                  vim.notify(
+                    "Debugger paused within 600 seconds, auto-resuming...",
+                    vim.log.levels.INFO
+                  )
+                end, 100)
+              end
+            end
+
+            dap.listeners.after.event_initialized["auto-continue"] = function()
+              debug_start_time = vim.loop.now()
+              vim.notify("Debugging session started", vim.log.levels.INFO)
+            end
+
+            dap.listeners.after.event_stopped["auto-continue"] = function()
+              auto_resume_if_stopped()
+            end
+
+            dap.listeners.after.event_terminated["auto-continue"] = function()
+              vim.notify("Debugging session ended", vim.log.levels.INFO)
+              debug_start_time = nil
+            end
+
             local is_windows = vim.fn.has("win32") > 0
             -- local path_sep = is_windows and "\\" or "/"
             local flutter_exec = is_windows and "flutter.bat" or "flutter"
-            local dap = require("dap")
             local flutterBin = vim.fn.resolve(vim.fn.exepath(flutter_exec))
             -- local flutterSdk = vim.fn.fnamemodify(flutterBin, ":h:h")
             -- local dartSdk = flutterSdk
