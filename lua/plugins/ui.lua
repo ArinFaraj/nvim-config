e4624e3e11337ed79da605d6ff90a6a974fefb94
Arin Faraj <arin.abdul99@gmail.com>
2024-11-21 16:22:10 +0300

    switch dashboard to snacks

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,33 +4,34 @@ return {
     opts = {
       options = {
         indicator = {
-          icon = "", -- this should be omitted if indicator style is not 'icon'
-          style = "icon",
+          -- icon = "", -- this should be omitted if indicator style is not 'icon'
+          -- style = "icon",
         },
       },
     },
   },
+  -- {
+  --   "folke/noice.nvim",
+  --   opts = function(_, opts)
+  --     table.insert(opts.routes, {
+  --       filter = {
+  --         event = "notify",
+  --         find = "No information available",
+  --       },
+  --       opts = {
+  --         skip = true,
+  --       },
+  --     })
+  --
+  --     opts.presets.lsp_doc_border = true
+  --   end,
+  -- },
   {
-    "folke/noice.nvim",
-    opts = function(_, opts)
-      table.insert(opts.routes, {
-        filter = {
-          event = "notify",
-          find = "No information available",
-        },
-        opts = {
-          skip = true,
-        },
-      })
-
-      opts.presets.lsp_doc_border = true
-    end,
-  },
-  {
-    "nvimdev/dashboard-nvim",
-    lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
-    opts = function(_, opts)
-      local logo = [[
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
 ⠀              ⠀⠀⠀⠀⠀⠀⠀⢰⡆⠀⠀⠀⠀⠀⠀⠀             ⠀
 ⠀⠀⠀⠀              ⠀⠀⠀⢠⣿⣿⡄⠀⠀⠀⠀⠀⠀             ⠀
 ⠀              ⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⡄⠀⠀⠀⠀⠀             ⠀
@@ -40,12 +41,11 @@ return {
 ⠀              ⠀⢠⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⡻⡄⠀⠀ |_  _|_     
 ⠀              ⣰⣿⣿⠿⠟⠛⠀⠀⠀⠀⠻⠿⠿⣿⣶⡄⠀ |_)  |_ \/\/
 ⠀         ⠀  ⠀⡠⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⢄⠀⠀          ⠀
-    ]]
-
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-
-      opts.config.header = vim.split(logo, "\n")
-      return opts
-    end,
+ ]],
+          -- stylua: ignore
+          ---@type snacks.dashboard.Item[]
+        },
+      },
+    },
   },
 }
